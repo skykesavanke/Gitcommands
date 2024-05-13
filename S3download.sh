6eb94a9e72464c07bc816a6804005eb16652ee5f
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-05-13 22:19:49 +0530

    new

--- S3download.sh
+++ S3download.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 BUCKET_NAME="hello-bucket-45"
-aws s3 cp s3://${BUCKET_NAME} ./
+download_dir="C:\Users\kesavank\Git practice s3 files"
+
+if ! which aws &> /dev/null; then
+  echo "Error: aws command not found. Please install the AWS CLI."
+  exit 1
+fi
+aws s3 rm s3://"${BUCKET_NAME}" --recursive
+aws s3 cp s3://"${BUCKET_NAME}"/"download_dir" --quiet
 echo "download completed successfully"
+echo "Deletion complete!"
