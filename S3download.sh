302efef0832c1613979ddbb4522a84b362efdede
root <root@IID2GYDY3>
2024-05-14 13:50:30 +0530

    new

--- S3download.sh
+++ S3download.sh
@@ -5,7 +5,7 @@ if ! which aws &> /dev/null; then
   echo "Error: aws command not found"
   exit 1
 fi
-aws s3 cp s3://"${BUCKET_NAME}" "${download_dir}" --quiet
+aws s3 cp "s3://${BUCKET_NAME}" "${download_dir}" --quiet
 
 echo "download completed successfully"
 echo "Deletion completed"
