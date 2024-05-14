d085fe963f2bb04465f9f7a0bf2a51bbd8e50158
root <root@IID2GYDY3>
2024-05-14 13:43:08 +0530

    new

--- S3download.sh
+++ S3download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BUCKET_NAME="keerthanas3"
-download_dir="//mnt//c//Users//kesavank//GitS3download"
+download_dir="/mnt/c/Users/kesavank/GitS3download"
 if ! which aws &> /dev/null; then
   echo "Error: aws command not found"
   exit 1
