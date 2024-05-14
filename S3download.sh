a0aed166a68ba80b6cdcf612f32627a6accf9963
root <root@IID2GYDY3>
2024-05-14 11:42:26 +0530

    new

--- S3download.sh
+++ S3download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-BUCKET_NAME="hello-bucket-45"
+BUCKET_NAME="keerthanas3"
 download_dir="C:\Users\kesavank\Git practice s3 files"
 if ! which aws &> /dev/null; then
   echo "Error: aws command not found"
   exit 1
 fi
 aws s3 cp s3://"${BUCKET_NAME}" "${download_dir}" --quiet
-aws s3 rm s3://"${BUCKET_NAME}" --recursive
+//aws s3 rm s3://"${BUCKET_NAME}" --recursive
 
 echo "download completed successfully"
 echo "Deletion completed"
