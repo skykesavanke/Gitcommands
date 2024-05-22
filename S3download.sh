ab85cb973312d74d4de53e713a7259a983071b6f
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-05-22 16:23:33 +0530

    updated

--- S3download.sh
+++ S3download.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 BUCKET_NAME="keerthanas3bucket"
 download_dir="/mnt/c/Users/kesavank/GitS3download"
+download="true"
 if ! which aws &> /dev/null; then
   echo "Error: aws command not found"
   exit 1
+elif [[ $download == "true"]];then
+  aws s3 cp "s3://${BUCKET_NAME}" "${download_dir}" --recursive --quiet
+  echo "Download completed successfully"
+else 
+
+  aws s3 rm "s3://${BUCKET_NAME}" --recursive
+  echo "Deletion completed"
 fi
-aws s3 cp "s3://${BUCKET_NAME}" "${download_dir}" --recursive --quiet
-echo "download completed successfully"
-aws s3 rm "s3://${BUCKET_NAME}" --recursive
-echo "Deletion completed"
