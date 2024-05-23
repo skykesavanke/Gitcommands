5983eb8525bd1e7482ac7ff3606fa48bb3be06fd
root <root@IID2GYDY3>
2024-05-23 15:52:41 +0530

    updated file

--- S3download.sh
+++ S3download.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
 BUCKET_NAME="keerthanas3bucket1"
 download_dir="/mnt/c/Users/kesavank/GitS3download"
-if [[ aws s3 ls "s3://${BUCKET_NAME}" -gt 0]]; then
-   aws s3 cp "s3://${BUCKET_NAME}" "${download_dir}" --recursive --quiet; then
+buckets=$(aws s3 ls "s3://$BUCKET_NAME/" --recursive | wc -l)
+echo "No of buckets: $buckets"
+if [ "$buckets" -gt 0 ]; then
+    aws s3 cp "s3://${BUCKET_NAME}" "${download_dir}" --recursive --quiet
     echo "Download completed successfully"
-  else
-    echo "Error: Download failed"
-    exit 1
-  fi
+    aws s3 rm "s3://${BUCKET_NAME}" --recursive
+else
+    echo "No files found"
+fi
 
