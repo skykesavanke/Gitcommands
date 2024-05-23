c77ce8c0fa2ab2fcf307abb6f8c297baed1cf7d5
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-05-23 12:38:06 +0530

    updated

--- S3download.sh
+++ S3download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BUCKET_NAME="keerthanas3bucket"
+BUCKET_NAME="keerthanas3bucket1"
 download_dir="/mnt/c/Users/kesavank/GitS3download"
-if [[ aws s3 ls -gt 0]]; then
+if [[ aws s3 ls "s3://${BUCKET_NAME}" -gt 0]]; then
    aws s3 cp "s3://${BUCKET_NAME}" "${download_dir}" --recursive --quiet; then
     echo "Download completed successfully"
   else
